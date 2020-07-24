f5aff69d3a68e4eb03a3feb0665916d6ea165dd2
Robert Au <19robertau132@gmail.com>
2020-07-24 15:41:53 -0700

    reset interface
    -removed js population of buttons, now buttons are in html
    -changed color scheme
    -no display functionality

--- script.js
+++ script.js
@@ -24,60 +24,14 @@ function operate(operator, a, b){
             break;
     }
 }
-//create button function
-function createButton(buttonObj){
-    let outbutton = document.createElement('button');
-    outbutton.id = buttonObj.id;
-    outbutton.innerHTML = buttonObj.text;
-    if(buttonObj.class) outbutton.classList.add(buttonObj.class);
-    grid.appendChild(outbutton);
-}
-//
-// create buttons as objects
-//
-const clearButton =     {id: "clear", text:"CA"};
-const nineButton =      {id: "nine", class:"num", text:"9"};
-const eightButton =     {id: "eight", class:"num", text:"8"};
-const sevenButton =     {id: "seven", class:"num", text:"7"};
-const divideButton =    {id: "divide", class:"operator", text:"/"};
-const sixButton =       {id: "six", class:"num", text:"6"};
-const fiveButton =      {id: "five", class:"num", text:"5"};
-const fourButton =      {id: "four", class:"num", text:"4"};
-const multiplyButton =  {id: "multiply", class:"operator", text:"*"};
-const threeButton =     {id: "three", class:"num", text:"3"};
-const twoButton =       {id: "two", class:"num", text:"2"};
-const oneButton =       {id: "one", class:"num", text:"1"};
-const subtractButton =  {id: "subtract", class:"operator", text:"-"};
-const zeroButton =      {id: "zero", class:"num", text:"0"};
-const decimalButton =   {id:"decimal", text:"."};
-const additionButton =  {id: "add", class:"operator", text:"+"};
-const equalButton =     {id:"equals", text:"="};
 
-const arrayButtons =[ clearButton, nineButton, eightButton, sevenButton,
-                    divideButton, sixButton, fiveButton, fourButton,
-                    multiplyButton, threeButton, twoButton, oneButton,
-                    subtractButton, zeroButton, decimalButton, additionButton,
-                    equalButton];
-
-//insert buttons into hmtl
-const grid = document.querySelector("#grid");
-arrayButtons.forEach(createButton);
 //
 // detecting input
 //
-var str =""; //holds input
-var operator;//holds type of operation
-// buttons appear on screen when clicked 
-const keys = document.querySelector("#grid");
-keys.addEventListener("click", e => {
-    if(e.target.matches('button')){
-        const key = e.target;
-        const keyClass = key.className;
-        if(keyClass == 'num'){
-            console.log("its a number");
-        }else if(keyClass == 'operator'){
-            console.log("its operator");
-        }
-        if(!keyClass){ console.log("idonk");}
-    }
-})
+document.onkeypress = function (e) {
+    e = e || window.event;
+    const key = e.key;
+    //console.log(key);
+    //window.alert(key);
+    
+};
