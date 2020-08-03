eb0d162f15188be797e62d8261050b9c8eaad43d
Robert Au <50808767+r6097@users.noreply.github.com>
2020-08-02 17:04:33 -0700

    Calculator is functional
    Restructured html and reworked css and js.
    Implemented Calculator class, works as the 'engine' that displays numbers and does operations.
    Event listeners for buttons that perform corresponding method and updates display afterwards.
    Previous operand appears at top right, while current operand is displayed in front.
    Pressing equals will perform operation of previous and current operand.

--- script.js
+++ script.js
@@ -1,37 +1,137 @@
-// math operations
-function add (a, b) {    return Number(a) + Number(b); }
-function subtract (a, b) {    return Number(a) - Number(b);}
-function multiply (a, b) {    return Number(a) * Number(b);}
-function divide (a, b) {    return Number(a) / Number(b);}
-//found online https://www.jacklmoore.com/notes/rounding-in-javascript/
-function round(value, decimals) {
-    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
-}
-//operator function
-function operate(operator, a, b){
-    switch (operator) {
-        case add:
-            add(a, b);
-            break;
-        case subtract:
-            subtract(a, b);
-            break;
-        case multiply:
-            multiply(a, b);
-            break;
-        case divide:
-            divide(a, b);
-            break;
+class Calculator {
+    constructor(prevOPText, currOPText) {
+        this.prevOPText = prevOPText;
+        this.currOPText = currOPText;
+    }
+
+    clear() {
+        this.currop = '';
+        this.prevop = '';
+        this.operation = undefined;
+    }
+
+    delete(){
+        this.currop = this.currop.toString().slice(0,1);
+    }
+
+    appendNumber(number){
+        // if currop is not defined, set it to empty string
+        if (!(this.currop)) this.currop = ''; 
+        // if number already contains '.' , then do nothing
+        if (number === '.'  && this.currop.includes('.')) return;
+        // otherwise append number
+        this.currop = this.currop.toString() + number.toString();
+    }
+
+    chooseOperation(operation){
+        // if there is nothing to operate on
+        if(!(this.currop)) return;
+        // if there was already an operation called previous to this one
+        // complete that first, then this operation
+        if(this.prevop) {
+            this.operate();
+        }
+        // execute operation
+        this.operation = operation;
+        this.prevop = this.currop;
+        this.currop = '';
+    }
+    // performs the math functions
+    operate(){
+        let answer;
+        const prev = parseFloat(this.prevop);
+        const curr = parseFloat(this.currop);
+        if (isNaN(prev) || isNaN(curr)) return;
+        switch (this.operation){
+            case '+':
+                answer = prev + curr;
+                break;
+            case '-':
+                answer = prev - curr;
+                break;
+            case '*':
+                answer = prev * curr;
+                break;
+            case '/':
+                answer = prev / curr;
+                break;
+            default:
+                return;        
+        }
+        this.currop = answer;
+        this.operation = undefined;
+        this.prevop = '';
+    }
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split('.')[0]);
+        const decimalDigits = stringNumber.split('.')[1];
+        let integerDisplay;
+        if (isNaN(integerDigits)) {
+            integerDisplay = '';
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0})
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`;
+        } else {
+            return integerDisplay;
+        }
+        
+    }
+
+    updateDisplay(){
+        this.currOPText.innerText = 
+            this.getDisplayNumber(this.currop);
+
+        if (this.operation) {
+            this.prevOPText.innerText =
+                 `${this.prevop} ${this.operation}`
+        } else {
+            this.prevOPText.innerText = '';
+        }
+
     }
 }
 
-//
-// detecting input
-//
-document.onkeypress = function (e) {
-    e = e || window.event;
-    const key = e.key;
-    //console.log(key);
-    //window.alert(key);
-    
-};
+// Select calculator buttons by data attribute and set into variables
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const clearButton = document.querySelector('[data-clear]');
+const prevOPText = document.querySelector('[data-prev-op]');
+const currOPText = document.querySelector('[data-curr-op]');
+
+const calculator = new Calculator(prevOPText, currOPText)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.operate();
+    calculator.updateDisplay();
+})
+
+clearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+})
